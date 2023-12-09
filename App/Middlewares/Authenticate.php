b93add5d14f211a5eecab131add4db4934884fe0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-09 11:19:08 +0330

    Update the authentication class login to avoid duplicate code

--- App/Middlewares/Authenticate.php
+++ App/Middlewares/Authenticate.php
@@ -2,41 +2,35 @@
 
 namespace App\Middlewares;
 
+use App\Core\Auth\Auth;
 use App\Middlewares\Contracts\MiddlewareInterface;
-use App\Models\User;
 
 class Authenticate implements MiddlewareInterface
 {
-   
+
     public function handle()
     {
         if (session_status() !== PHP_SESSION_ACTIVE) {
             session_start();
         }
+        
+        $currentRoute = $_SERVER['REQUEST_URI'];
+        $loginRoute = site_url(); // Adjust this based on your actual login route
 
-        // Check if the user is logged in
-        if (!isset($_SESSION['user_id'])) {
-            $currentRoute = $_SERVER['REQUEST_URI'];
-            $loginRoute = '/login'; // Adjust this based on your actual login route
-
-            // Redirect to the login page if not already there
-            if ($currentRoute !== $loginRoute) {
-                header('Location: ' . $loginRoute);
-                exit();
-            }
-        } else {
-            // Optionally, you can remove this line if the User instance is not needed
-            $this->user = new User($_SESSION['username']);
-
-            // Check if the current route is the login route
-            $currentRoute = $_SERVER['REQUEST_URI'];
-            $loginRoute = '/login'; // Adjust this based on your actual login route
-
-            // Redirect to the home page if trying to access the login page
-            if ($currentRoute == $loginRoute) {
-                header('Location: /');
-                exit();
-            }
+        // Check if the user is not logged in and not on the login route
+        if (!Auth::check() && $currentRoute !== $loginRoute) {
+            $this->redirect($loginRoute);
         }
+
+        // Check if the user is logged in and trying to access the login route
+        if (Auth::check() && $currentRoute == $loginRoute) {
+            $this->redirect('/');
+        }
+    }
+
+    private function redirect($location)
+    {
+        header('Location: ' . $location);
+        exit();
     }
 }
