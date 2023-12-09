e27fba6bc14ae3a1740a279eeaf6fb895c0fef35
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-09 09:33:35 +0330

    Fixed the files name conflation

---
+++ App/Middlewares/Authenticate.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Middlewares;
+
+use App\Middlewares\Contracts\MiddlewareInterface;
+use App\Models\User;
+
+class Authenticate implements MiddlewareInterface
+{
+    public function handle()
+    {
+        global $user;
+
+        // Start the session (if not started already)
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        // Check if the user is logged in
+        if (!isset($_SESSION['user_id'])) {
+
+            $currentRoute = $_SERVER['REQUEST_URI'];
+            $loginRoute = '/login'; // Adjust this based on your actual login route
+            if ($currentRoute !== $loginRoute) {
+                header('Location: /login');
+                exit();
+            }
+        } else {
+            $user = new User($_SESSION['username']);
+            // Check if the current route is the login route
+            $currentRoute = $_SERVER['REQUEST_URI'];
+            $loginRoute = '/login'; // Adjust this based on your actual login route
+
+            if ($currentRoute == $loginRoute) {
+                header('Location: /');
+                exit();
+            }
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Core\Routing\Route;
-use App\Middlewares\Authorize;
+use App\Middlewares\Authenticate;
 
 // Landing page route
-Route::get('/', ['DashboardController', 'index'], [Authorize::class]);
+Route::get('/', ['DashboardController', 'index'], [Authenticate::class]);
 
 // Authentication routes
-Route::get('/login', ['LoginController', 'index'], [Authorize::class]);
+Route::get('/login', ['LoginController', 'index'], [Authenticate::class]);
 Route::post('/login/check', ['LoginController', 'validate']);
 Route::post('/logout', ['LoginController', 'logout']);
