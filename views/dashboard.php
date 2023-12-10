b861e8cb952df7d2d828888fc6a1854c18445955
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-10 16:01:27 +0330

    Add the js function to show the login notification and them hide

--- views/dashboard.php
+++ views/dashboard.php
@@ -41,15 +41,15 @@ require_once BASE_PATH . 'views/inventory/components/aside.php';
         </div>
     </div>
 
-    
+
     <div id="toast-simple" class="fixed bottom-5 left-5 flex justify-between items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse rounded-lg shadow text-gray-400 space-x bg-gray-800" role="alert">
         <img class="w-10 h-10 rounded-full" src="<?= asset_url('img/default.jpg') ?>" alt="profile Photo" />
         <p class="text-sm font-normal"><?= $user['name'] . " " . $user['family'] ?> عزیز خوش آمدید.</p>
-        <svg class="cursor-pointer" width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" onclick="toggleSidebar()">
+        <svg id="close_login_notification" class="cursor-pointer" width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
             <path fill="red" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z" />
         </svg>
     </div>
-    
+
 </section>
 
 <script src="<?= asset_url('js/handleLoginNotification.js') ?>"></script>
@@ -72,6 +72,20 @@ require_once BASE_PATH . 'views/inventory/components/aside.php';
 
             sendAjaxRequestForSuccessfulLogin(url, id, username, time, host, ip);
         }
+    });
+
+    $(document).ready(function() {
+        // Set the initial left position to '10px'
+        $('#toast-simple').css('left', '10px');
+
+        // Use setTimeout to change the left position to '-100%' after 5 seconds
+        setTimeout(function() {
+            $('#toast-simple').css('left', '-100%');
+        }, 5000);
+
+        $('#close_login_notification').click(function() {
+            $('#toast-simple').css('left', '-100%');
+        })
     });
 </script>
 
