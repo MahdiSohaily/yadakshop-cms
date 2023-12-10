a5b76729b7a42ce82dba64984c07e7831b1ac6f4
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-10 15:47:56 +0330

    Add the login failer notification message

--- views/dashboard.php
+++ views/dashboard.php
@@ -5,13 +5,51 @@ require_once BASE_PATH . 'views/inventory/components/aside.php';
 ?>
 
 <section class="mt-14 p-5">
-    <div id="toast-simple" class="fixed bottom-5 left-11 flex justify-between items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse rounded-lg shadow text-gray-400 space-x bg-gray-800" role="alert">
+    <!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com -->
+    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
+        <div class="-m-1 flex flex-wrap md:-m-2">
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+                </div>
+            </div>
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+                </div>
+            </div>
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+                </div>
+            </div>
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+                </div>
+            </div>
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp" />
+                </div>
+            </div>
+            <div class="flex w-1/3 flex-wrap">
+                <div class="w-full p-1 md:p-2">
+                    <img alt="gallery" class="block h-full w-full rounded-lg object-cover object-center" src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <div id="toast-simple" class="fixed bottom-5 left-5 flex justify-between items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse rounded-lg shadow text-gray-400 space-x bg-gray-800" role="alert">
         <img class="w-10 h-10 rounded-full" src="<?= asset_url('img/default.jpg') ?>" alt="profile Photo" />
         <p class="text-sm font-normal"><?= $user['name'] . " " . $user['family'] ?> عزیز خوش آمدید.</p>
         <svg class="cursor-pointer" width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" onclick="toggleSidebar()">
             <path fill="red" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z" />
         </svg>
     </div>
+    
 </section>
 
 <script src="<?= asset_url('js/handleLoginNotification.js') ?>"></script>
