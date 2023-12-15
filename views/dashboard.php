4701b3413fa57b2152ad65bf447eafa5438dfbe0
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-15 18:56:53 +0330

    Add the dashboard page cards

--- views/dashboard.php
+++ views/dashboard.php
@@ -5,10 +5,74 @@ require_once BASE_PATH . 'views/inventory/components/aside.php';
 ?>
 
 <section class="mt-14 p-5">
+    <!-- Start Content -->
+    <div class="grid grid-cols-1 gap-5 mt-6 sm:grid-cols-2 lg:grid-cols-4">
+        <div x-for="i in 4" :key="i">
+            <div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+                <div class="flex items-start justify-between">
+                    <div class="flex flex-col space-y-2">
+                        <span class="text-gray-400">مجموع کاربران</span>
+                        <span class="text-lg font-semibold">100</span>
+                    </div>
+                    <img class="rounded-md w-16 h-16" src="<?= asset_url('icons/user.svg') ?>" alt="">
+                </div>
+                <div>
+                    <span class="inline-block px-2 text-sm text-white bg-green-300 rounded">14%</span>
+                    <span>مدیریت کاربران</span>
+                </div>
+            </div>
+        </div>
+        <div x-for="i in 4" :key="i">
+            <div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+                <div class="flex items-start justify-between">
+                    <div class="flex flex-col space-y-2">
+                        <span class="text-gray-400">مجموع فاکتور های ثبت شده</span>
+                        <span class="text-lg font-semibold">100,221</span>
+                    </div>
+                    <img class="rounded-md w-16 h-16" src="<?= asset_url('icons/invoice.svg') ?>" alt="">
+                </div>
+                <div>
+                    <span class="inline-block px-2 text-sm text-white bg-green-300 rounded">14%</span>
+                    ثبت فاکتور جدید
+                </div>
+            </div>
+        </div>
+        <div x-for="i in 4" :key="i">
+            <div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+                <div class="flex items-start justify-between">
+                    <div class="flex flex-col space-y-2">
+                        <span class="text-gray-400">مجموع اقلام وارد شده</span>
+                        <span class="text-lg font-semibold">100,221</span>
+                    </div>
+                    <img class="rounded-md w-16 h-16" src="<?= asset_url('icons/receive.svg') ?>" alt="">
+                </div>
+                <div>
+                    <span class="inline-block px-2 text-sm text-white bg-green-300 rounded">14%</span>
+                    <span>گزارش اقلام وارده</span>
+                </div>
+            </div>
+        </div>
+        <div x-for="i in 4" :key="i">
+            <div class="p-4 transition-shadow border rounded-lg shadow-sm hover:shadow-lg">
+                <div class="flex items-start justify-between">
+                    <div class="flex flex-col space-y-2">
+                        <span class="text-gray-400">مجموع اقلام به فروش رسیده</span>
+                        <span class="text-lg font-semibold">100,221</span>
+                    </div>
+                    <img class="rounded-md w-16 h-16" src="<?= asset_url('icons/deliver.svg') ?>" alt="">
+                </div>
+                <div>
+                    <span class="inline-block px-2 text-sm text-white bg-green-300 rounded">14%</span>
+                    <span>گزارش اقلام خارجه</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php
 if (isset($_SESSION['login_status'])) :
 ?>
+
     <div id="toast-simple" class="fixed bottom-5 left-5 flex justify-between items-center w-full max-w-xs p-4 space-x-4 rtl:space-x-reverse rounded-lg shadow text-gray-400 space-x bg-gray-800" role="alert">
         <img class="w-10 h-10 rounded-full" src="<?= asset_url('img/default.jpg') ?>" alt="profile Photo" />
         <p class="text-sm font-normal"><?= $user['name'] . " " . $user['family'] ?> عزیز خوش آمدید.</p>
